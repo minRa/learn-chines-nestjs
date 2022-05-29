d2ca82c4f268defe95123cf2fe520956933009c4
minRa <over6508@naver.com>
2022-05-29 19:35:40 +1200

    add return type in decorator

--- src/common/decorators/user.decorator.ts
+++ src/common/decorators/user.decorator.ts
@@ -1,7 +1,8 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { User } from 'src/models/user.schema';
 
 export const CurrentUser = createParamDecorator(
-  (data: unknown, ctx: ExecutionContext) => {
+  (data: unknown, ctx: ExecutionContext): User => {
     const request = ctx.switchToHttp().getRequest();
     return request.user;
   },
